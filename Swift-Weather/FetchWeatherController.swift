e46d56f2bb102cc9f8f4a3ee0973aef636c32503
Mike Mannix <mike.mannix@gmail.com>
2016-03-06 21:52:23 -0500

    Use core location

--- Swift-Weather/FetchWeatherController.swift
+++ Swift-Weather/FetchWeatherController.swift
@@ -7,24 +7,44 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FetchWeatherController: UIViewController {
+class FetchWeatherController: UIViewController, CLLocationManagerDelegate {
 
     var data: AnyObject?
+    var lat = "40.1024362"
+    var lon = "-83.1483597"
+
+    var locationManager: CLLocationManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.startUpdatingLocation()
+        }
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if let coordinate = manager.location?.coordinate {
+            lat = "\(coordinate.latitude)"
+            lon = "\(coordinate.longitude)"
+        }
         getWeather()
     }
 
     func getWeather() {
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        let url = NSURL(string: "http://forecast.weather.gov/MapClick.php?lat=40.1024362&lon=-83.1483597&FcstType=json")
+        let url = NSURL(string: "http://forecast.weather.gov/MapClick.php?lat=\(lat)&lon=\(lon)&FcstType=json")
         session.dataTaskWithURL(url!) { (data, response, error) in
             if error == nil {
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -36,6 +56,8 @@ class FetchWeatherController: UIViewController {
             }
         }.resume()
     }
+
+
 
 
     // MARK: - Navigation
